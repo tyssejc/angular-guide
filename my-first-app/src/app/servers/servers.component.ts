be0dff2a60b24e3dc9adef7e1924181df9f722bd
tyssejc <tyssejc@gmail.com>
2019-10-27 14:53:44 -0400

    end of ngFor video

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -13,6 +13,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created';
   serverName = 'DefaultValue';
   serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
   constructor() {
     setTimeout(() => {
@@ -24,8 +25,9 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created! Name is '+this.serverName;
     this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name is '+this.serverName;
   }
 
   onUpdateServerName(event: any) {
