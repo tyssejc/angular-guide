06d8c161c1768c293ad68323e704a9ac077cdce2
tyssejc <tyssejc@gmail.com>
2019-11-03 08:50:36 -0500

    completed assignment

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  displayText = false;
+  log = [];
+
+  onToggleDetails() {
+    this.displayText = !this.displayText;
+    // this.log.push(this.log.length + 1);
+    this.log.push(new Date());
+  }
 }
